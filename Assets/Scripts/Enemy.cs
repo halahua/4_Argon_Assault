fe89090ab5bf9333bdd426dea7d02848a9d3b701
João Traini <joao.traini@gmail.com>
2019-07-06 18:40:02 -0300

    4_AS_101

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
 
-    
+
+    private void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;        
+    }
+
     void OnParticleCollision(GameObject other)
     {
         Destroy(gameObject);
